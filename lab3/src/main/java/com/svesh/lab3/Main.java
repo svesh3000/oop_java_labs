22a7a5a471736e0cad39bf9f9d8f26ddec92765c
Sveshnikov Michail <russkij2006@mail.ru>
2026-01-15 11:16:03 +0300

    [LAB-3] Demonstration

--- lab3/src/main/java/com/svesh/lab3/Main.java
+++ lab3/src/main/java/com/svesh/lab3/Main.java
@@ -1,7 +1,56 @@
 package com.svesh.lab3;
 
+import java.util.*;
+import com.svesh.lab3.animals.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Lab3!");
+        List<Mammal> mammals = Arrays.asList(
+                new Hedgehog("Hedgehog1"),
+                new Manul("Manul1"),
+                new Lynx("Lunx1"),
+                new Hedgehog("Hedgehog2"),
+                new Manul("Manul2")
+        );
+
+        System.out.println("1. segregate(Mammals, Erinaceidae, Felidae, Carnivores):");
+        List<Erinaceidae> collection1 = new ArrayList<>();
+        List<Felidae> collection2 = new ArrayList<>();
+        List<Carnivore> collection3 = new ArrayList<>();
+
+        AnimalSegregator.segregate(mammals, collection1, collection2, collection3);
+        printCollections(collection1, collection2, collection3);
+
+        System.out.println("\n2. segregate(<Carnivores, Chordates, Manuls, Felidae):");
+        List<Carnivore> carnivores = Arrays.asList(
+                new Lynx("Lunx1"),
+                new Manul("Manul1"),
+                new Manul("Manul2")
+        );
+        List<Chordate> col1 = new ArrayList<>();
+        List<Manul> col2 = new ArrayList<>();
+        List<Felidae> col3 = new ArrayList<>();
+
+        AnimalSegregator.segregate(carnivores, col1, col2, col3);
+        printCollections(col1, col2, col3);
+
+        System.out.println("\n3. segregate(Erinaceidae, Insectivores, Carnivores, Carnivores):");
+        List<Erinaceidae> erinaceidae = Arrays.asList(
+                new Hedgehog("Hedgehog1"),
+                new Hedgehog("Hedgehog2")
+        );
+        List<Insectivore> c1 = new ArrayList<>();
+        List<Carnivore> c2 = new ArrayList<>();
+        List<Carnivore> c3 = new ArrayList<>();
+
+        AnimalSegregator.segregate(erinaceidae, c1, c2, c3);
+        printCollections(c1, c2, c3);
+    }
+
+    private static void printCollections(Collection<?>... collections) {
+        String[] names = {"Hedgehogs:", "Manuls:", "Lunxes:"};
+        for (int i = 0; i < collections.length; i++) {
+            System.out.println(names[i] + " " + collections[i]);
+        }
     }
 }
